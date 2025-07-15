0f0b68138cbf871ebfb955371cb2aefb82cf68e6
aakashsahh <aakashsah181@gmail.com>
2025-07-15 23:46:52 +0545

    defined theme for the app

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  useMaterial3: true,
+  textTheme: GoogleFonts.robotoTextTheme(),
+);
